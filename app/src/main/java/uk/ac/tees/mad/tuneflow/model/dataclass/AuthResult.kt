755ab0a07481626c9004c49dd323891babde528f
Dheeraj-nama <203520887+Dheeraj-Nama28@users.noreply.github.com>
2025-03-29 11:23:48 +0000

    commit 11

--- app/src/main/java/uk/ac/tees/mad/tuneflow/model/dataclass/AuthResult.kt
+++
@@ -1,7 +0,0 @@
-package uk.ac.tees.mad.tuneflow.model.dataclass
-
-sealed class AuthResult<out T> {
-    data object Loading : AuthResult<Nothing>()
-    data class Success<out T>(val data: T) : AuthResult<T>()
-    data class Error(val exception: Exception) : AuthResult<Nothing>()
-}
--- app/src/main/java/uk/ac/tees/mad/tuneflow/model/dataclass/UiState.kt
+++
@@ -1,7 +0,0 @@
-package uk.ac.tees.mad.tuneflow.model.dataclass
-
-sealed class UiState {
-    object Loading : UiState()
-    data class Success(val response: ApiPlaylistResponse) : UiState()
-    data class Error(val errorState: ErrorState, val message: String) : UiState()
-}
